8d88b2b69f653b1067835126c41828f562e7433d
MuhammedZeki <muhammed.codes@gmail.com>
2026-03-02 00:48:49 +0300

    Added Admin Areas and About

---
+++ OnlineEdu.UI/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.UI.Areas.Admin.Controllers
+{
+
+
+    [Area("Admin")]
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- OnlineEdu.UI/Program.cs
+++ OnlineEdu.UI/Program.cs
@@ -26,4 +26,12 @@ app.MapControllerRoute(
     .WithStaticAssets();
 
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.Run();
