0bebf462999d08a145cf39cbec626886ab6bcbc3
MuhammedZeki <muhammed.codes@gmail.com>
2026-03-02 17:41:25 +0300

    Added HttpClient

--- OnlineEdu.UI/Areas/Admin/Controllers/AboutController.cs
+++ OnlineEdu.UI/Areas/Admin/Controllers/AboutController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.UI.DTOs.AboutDTOs;
 
 namespace OnlineEdu.UI.Areas.Admin.Controllers
 {
@@ -6,10 +7,18 @@ namespace OnlineEdu.UI.Areas.Admin.Controllers
 
     [Area("Admin")]
     public class AboutController : Controller
-    {
-        public IActionResult Index()
+    {   
+        private readonly HttpClient _httpClient;
+
+        public AboutController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClient = httpClientFactory.CreateClient("OnlineEduApi");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values =await _httpClient.GetFromJsonAsync<List<ResultAboutDto>>("abouts");
+            return View(values);
         }
     }
 }
--- OnlineEdu.UI/Program.cs
+++ OnlineEdu.UI/Program.cs
@@ -1,7 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddHttpClient();
+builder.Services.AddHttpClient("OnlineEduApi", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7134/api/");
+});
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
